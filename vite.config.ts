7eb7d8cc4e0b02daa4b456ed17dac0225cf8120f
fatihua <fatihua@gmail.com>
2026-01-07 11:31:38 +0700

    feat: manual integration of shadcn/ui and tailwindcss v3

--- vite.config.ts
+++ vite.config.ts
@@ -9,5 +9,10 @@ export default defineConfig({
         keycloakify({
             accountThemeImplementation: "none"
         })
-    ]
+    ],
+    resolve: {
+        alias: {
+            "@": "/src"
+        }
+    }
 });
